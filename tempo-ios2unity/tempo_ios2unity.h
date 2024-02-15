3673171b8a6a2527b3b31c7cf47a1d9e57064404
omnibaker <37693234+omnibaker@users.noreply.github.com>
2024-02-15 12:14:12 +1100

    Initial Commit

---
+++ tempo-ios2unity/tempo_ios2unity.h
@@ -0,0 +1,18 @@
+//
+//  tempo_ios2unity.h
+//  tempo-ios2unity
+//
+//  Created by Stephen Baker on 15/2/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for tempo_ios2unity.
+FOUNDATION_EXPORT double tempo_ios2unityVersionNumber;
+
+//! Project version string for tempo_ios2unity.
+FOUNDATION_EXPORT const unsigned char tempo_ios2unityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tempo_ios2unity/PublicHeader.h>
+
+
